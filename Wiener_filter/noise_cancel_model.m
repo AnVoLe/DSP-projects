41af45348eb922c67b48160fc31210394e5278f8
AnVoLe <volean97@gmail.com>
2021-09-26 21:15:45 -0700

    Update noise_cancel_model.m

--- Wiener_filter/noise_cancel_model.m
+++ Wiener_filter/noise_cancel_model.m
@@ -5,36 +5,36 @@ clear, close all
 n1=[0:500]';
 vn=randn(size(n1));
 
-tmp=filter(1,[1 -0.8],vn); % generate 501 samples
-v1n=tmp(301:500);       %keep only the last 200 (avoids transients)
-tmp=filter(1,[1 0.6],vn);    % same for v2n
-v2n=tmp(301:500);
+tmp1=filter(1,[1 -0.8],vn); % generate 501 samples
+v1n=tmp1(301:500);       %keep only the last 200 (avoids transients)
+tmp2=filter(1,[1 0.6],vn);    % same for v2n
+v2n=tmp2(301:500);
 
 w0=0.05*pi;
 n=[0:199]';
 sn=sin(w0*n);       % phi is any value between 0 and 2pi, phi=0 is chosen
 dn=sn+v1n;      %noisy sinusoid;primary signal
-xn=v2n;         %correlated noise; secondary signal
 
+xn=v2n;         %correlated noise; secondary signal
 % estimate from the data rxx and rxd
 N=length(xn);
 rxx=xcorr(xn,xn)/N;     %biased correlation estimator
-rxd=xcorr(xn,dn)/N;     %cross-correlation estimator
+rxd=xcorr(dn,xn)/N;     %cross-correlation estimator
 
 M=12;            % Wiener filter order
-rxx=rxx(N:N+M); % extract rxx(0),rxx(1),...,rxx(M)
-rxd=rxd(N:N+M); % extract rxd(0),rxd(1),...,rxd(M)
-R=toeplitz(rxx); % correlation matrix
-ho=R\rxd;       % solve for the Wiener filter coeffs
+rxx=rxx(N:N+M); % extract rxx(0),rxx(1),...,rxx(M) because xcorr finds negative lags as well
+rxd=rxd(N:N+M) % extract rxd(0),rxd(1),...,rxd(M)
+R=toeplitz(rxx) % correlation matrix
+ho=R\rxd       % solve for the Wiener filter coeffs
 
 yn=filter(ho,1,xn); % filter xn through ho
-en=dn-yn;       % en is the cleaned dn here
+en=dn-yn;       % en is the cleaned sn here
 
 % plot results
 subplot(3,1,1),plot(n,sn,'b',n,dn,'-r')
 title(['Wiener Filter of order M=',num2str(M)])
-subplot(3,1,2),plot(n,xn,'-b')
+subplot(3,1,2),plot(n,v1n,'-b',n,yn,'-r')
 axis([0 200 -5 5])
 subplot(3,1,3),plot(n,sn,'.b',n,en,'-r')
-axis([0 200 -2 2])
+axis([0 200 -3 3])
 
