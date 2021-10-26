33e4b52d5cf2cf81bd2e7189fe6f1bbcce23c5e0
AnVoLe <volean97@gmail.com>
2021-10-26 12:30:41 -0700

    Update noise_cancel_model.m

--- Wiener_filter/noise_cancel_model.m
+++ Wiener_filter/noise_cancel_model.m
@@ -12,7 +12,7 @@ v2n=tmp2(301:500);
 
 w0=0.05*pi;
 n=[0:199]';
-sn=sin(w0*n);       % phi is any value between 0 and 2pi, phi=0 is chosen
+sn=2*sin(w0*n);       % phi is any value between 0 and 2pi, phi=0 is chosen
 dn=sn+v1n;      %noisy sinusoid;primary signal
 
 xn=v2n;         %correlated noise; secondary signal
@@ -23,9 +23,9 @@ rxd=xcorr(dn,xn)/N;     %cross-correlation estimator
 
 M=12;            % Wiener filter order
 rxx=rxx(N:N+M); % extract rxx(0),rxx(1),...,rxx(M) because xcorr finds negative lags as well
-rxd=rxd(N:N+M) % extract rxd(0),rxd(1),...,rxd(M)
-R=toeplitz(rxx) % correlation matrix
-ho=R\rxd       % solve for the Wiener filter coeffs
+rxd=rxd(N:N+M); % extract rxd(0),rxd(1),...,rxd(M)
+R=toeplitz(rxx); % correlation matrix
+ho=R\rxd   ;    % solve for the Wiener filter coeffs
 
 yn=filter(ho,1,xn); % filter xn through ho
 en=dn-yn;       % en is the cleaned sn here
@@ -33,7 +33,7 @@ en=dn-yn;       % en is the cleaned sn here
 % plot results
 subplot(3,1,1),plot(n,sn,'b',n,dn,'-r')
 title(['Wiener Filter of order M=',num2str(M)])
-subplot(3,1,2),plot(n,v1n,'-b',n,yn,'-r')
+subplot(3,1,2),plot(n,dn,'-b',n,yn,'-r',n,v1n,'-g')
 axis([0 200 -5 5])
 subplot(3,1,3),plot(n,sn,'.b',n,en,'-r')
 axis([0 200 -3 3])
